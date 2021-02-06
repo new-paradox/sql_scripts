4878b12065a58d70fcd80da309a65eedab81b5c2
new-paradox <70213448+new-paradox@users.noreply.github.com>
2021-02-07 00:23:39 +0600

    add sql scrip create db

---
+++ create_db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE example;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(255));
+INSERT INTO users VALUES
+  (DEFAULT, 'Вася'),
+  (DEFAULT, 'Петя':);
