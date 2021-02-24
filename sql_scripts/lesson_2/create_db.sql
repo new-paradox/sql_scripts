cd32833d3052c63f100f321d3965ef99fc2cdf6c
new-paradox <70213448+new-paradox@users.noreply.github.com>
2021-02-24 18:19:22 +0600

    Delete sql_scripts directory

--- sql_scripts/lesson_2/create_db.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE example;
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(255));
-INSERT INTO users VALUES
-  (DEFAULT, 'Вася'),
-  (DEFAULT, 'Петя':);
